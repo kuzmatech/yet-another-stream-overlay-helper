4d8c29984b715a8806def7289da4b0ea6206b30c
Samuel Kerr <kuzmatech@gmail.com>
2017-12-10 02:02:15 +0000

    Added electron-vue boilerplate.

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'yet-another-stream-overlay-helper'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
